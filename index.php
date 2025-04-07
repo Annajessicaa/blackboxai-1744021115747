16ffa9d98c0e47b7c6443d0ab6e4106da25fa15c
Annajessicaa <anastasiajessica2412@gmail.com>
2025-04-07 18:18:48 +0800

    Add index.php 1744021129533

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<div class="container mx-auto p-8">
+    <h2 class="text-2xl mb-4">Selamat Datang di Secure File Manager</h2>
+    <p>Aplikasi ini memungkinkan Anda untuk mengenkripsi dan mendekripsi file teks dengan aman menggunakan algoritma AES dan RSA.</p>
+    <p>Silakan pilih menu di atas untuk mulai menggunakan aplikasi.</p>
+</div>
+</body>
+</html>
