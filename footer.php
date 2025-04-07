0e4b8135fb910219a6dc11dbb74e997fb0c748a2
Annajessicaa <anastasiajessica2412@gmail.com>
2025-04-07 18:18:46 +0800

    Add footer.php 1744021127018

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-gray-100 p-4 mt-8">
+    <div class="container mx-auto text-center text-gray-600">
+        &copy; <?= date('Y') ?> Secure File Manager
+    </div>
+</footer>
+</body>
+</html>
